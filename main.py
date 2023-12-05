ac1ed5433ab39cd3b4eb4ddcbde693d0fc772881
ramiro <ramiro@tinybird.co>
2023-12-05 11:56:28 +0100

    fix read ndjson

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import click
 @click.command()
 @click.option('--interval', default=1, help='Interval (in hours) for data extraction')
 @click.option('--format', default='ndjson', help='Format for saving data')
+@click.option('--save', default=True, help='Save data')
 @click.option('--send', default=True, help='Send data to Tinybird')
 
-def main(interval, format, send):
+def main(interval, format, save, send):
     # Specify start and end times for data extraction
     start_time = dt.datetime.utcnow()
     end_time = start_time + dt.timedelta(hours=interval)
@@ -23,11 +24,12 @@ def main(interval, format, send):
     satellite_data = sats.convert_tle_to_coordinates(satellites, start_time, end_time)
 
     # Save data
-    data.save_data(satellite_data, format=format, filename='satellite_data')
+    if save:
+        data.save_data(satellite_data, format=format, filename='satellite_data')
 
     # Send data to Tinybird
     if send:
-        data.send_satellite_data(satellite_data)
+        data.send_satellite_data(filename='satellite_data.ndjson')
 
 if __name__ == "__main__":
     main()
